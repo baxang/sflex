bb1380a48acb6a92e727cd1fac174cda7d5bbd6e
Sanghyun Park <sh@baxang.com>
2016-06-14 00:16:13 +1000

    Use HD=1 page when crawl.

--- config/clock.rb
+++ config/clock.rb
@@ -14,7 +14,7 @@ module Clockwork
   end
   every(5.minutes, 'episode.media', tz: 'UTC') do
     ::Episode.favourite.unvisited.limit(1).each do |episode|
-      crawler = ::EpisodeCrawler.new(episode.uri, episode)
+      crawler = ::EpisodeCrawler.new(episode.uri + '&HD=1', episode)
       crawler.run
     end
   end
