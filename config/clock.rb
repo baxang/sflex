6fa20fd0914faaa8207b8b5085f967d9fee16e9d
Sanghyun Park <sh@baxang.com>
2016-06-19 17:00:59 +1000

    Shorten crawl frequency.

--- config/clock.rb
+++ config/clock.rb
@@ -7,12 +7,12 @@ module Clockwork
     puts "Running #{job}"
   end
 
-  every(30.minutes,    'new.episodes',  tz: 'UTC') do
+  every(5.minutes,    'new.episodes',  tz: 'UTC') do
     source = 'http://joovideo.com'
     crawler = ::HomeCrawler.new(source)
     crawler.run
   end
-  every(3.minutes, 'episode.media', tz: 'UTC') do
+  every(1.minutes, 'episode.media', tz: 'UTC') do
     ::Episode.favourite.unvisited.limit(1).each do |episode|
       crawler = ::EpisodeCrawler.new(episode.uri + '&HD=1', episode)
       crawler.run
